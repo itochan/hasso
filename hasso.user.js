a662ac27d4d45b498cf70654c89a72c49ced044d
Kazunori Jo (itochan) <itochan315@gmail.com>
2012-06-12 14:54:04 +0900

    add load jquery function

--- hasso.user.js
+++ hasso.user.js
@@ -5,3 +5,14 @@
 // @author       itochan
 // @description  その発送はなかった
 // ==/UserScript==
+
+function load_jquery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
