58cd8d75791c34f15d3f32ce54a4fda6bc8b776f
Kazunori Jo (itochan) <itochan315@gmail.com>
2012-06-12 14:47:06 +0900

    add headers

---
+++ hasso.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         hasso.user.js
+// @namespace    hasso
+// @include      https://www.amazon.co.jp/gp/css/*history*
+// @author       itochan
+// @description  その発送はなかった
+// ==/UserScript==
