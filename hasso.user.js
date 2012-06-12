a344607485280e3ce6508f9011366a510581021a
Kazunori Jo (itochan) <itochan315@gmail.com>
2012-06-12 15:30:30 +0900

    bump version to 1.0

--- hasso.user.js
+++ hasso.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         hasso.user.js
 // @namespace    hasso
+// @version      1.0
 // @include      https://www.amazon.co.jp/gp/css/*history*
 // @author       itochan
 // @description  その発送はなかった
